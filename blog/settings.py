9b9d6199bc93e80999669576434cf6d2c8dc356a
marwan-alshalabi <alshalabimarwan1@gmail.com>
2024-01-15 08:39:13 +0100

    add template inheritance

--- blog/settings.py
+++ blog/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
